36ecb7ef1ef18197071849e4b3a4a437e7014d3b
widetrace <widetrace@icloud.com>
2021-10-04 15:16:39 +0300

    Update titleSearch.ts
    • Поиск по названию книги дописан

--- src/scenes/titleSearch.ts
+++ src/scenes/titleSearch.ts
@@ -1,15 +1,65 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
 
-import { Scenes } from 'telegraf';
+import axios from 'axios';
+import { Markup, Scenes } from 'telegraf';
 import Bot from '../types/bot';
 
+interface bookObj {
+  id: number,
+  name: string,
+  short: string,
+  author: number,
+}
+
 export default class titleSearch {
+  private static ACTIONS = {
+    findAnotherOne: 'FIND_ANOTHER_TITLE',
+    exitOnMain: 'EXIT_FROM_TITLE_SEARCH',
+  }
+
+  static BUTTONS = [
+    [Markup.button.callback('Найти книгу по названию', titleSearch.ACTIONS.findAnotherOne)],
+    [Markup.button.callback('Вернуться на главную', titleSearch.ACTIONS.exitOnMain)],
+  ]
+
   static init(): Scenes.BaseScene<Bot.IContext> {
     const scene = new Scenes.BaseScene<Bot.IContext>('titleSearch');
 
-    scene.enter((ctx) => {
-      console.log('touch');
+    scene.enter(async (ctx) => {
+      ({ message_id: ctx.session.prevMessage } = await ctx.reply('Введите название книги'));
+    });
+
+    scene.on('text', async (ctx) => {
+      let book: Array<bookObj>;
+
+      await ctx.deleteMessage(ctx.session.prevMessage);
+
+      try {
+        book = (await axios.get(encodeURI(`http://localhost:3000/books?title=${ctx.message}`))).data;
+      } catch (err) {
+        ctx.scene.enter('start');
+        throw new Error(err);
+      }
+
+      if (!book.length) {
+        ctx.reply('Такой книги нет, или название неверно',
+          Markup.inlineKeyboard(titleSearch.BUTTONS));
+      }
+
+      ctx.deleteMessage(ctx.message.message_id);
+      ctx.reply(book[0].short, Markup.inlineKeyboard(titleSearch.BUTTONS));
+    });
+
+    scene.action(titleSearch.ACTIONS.findAnotherOne, async (ctx) => {
+      await ctx.deleteMessage(ctx.callbackQuery.message.message_id);
+
+      ctx.scene.reenter();
+    });
+
+    scene.action(titleSearch.ACTIONS.exitOnMain, async (ctx) => {
+      await ctx.deleteMessage(ctx.callbackQuery.message.message_id);
+
       ctx.scene.enter('start');
     });
 
